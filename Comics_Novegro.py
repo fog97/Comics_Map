642dff90c329d90e6821ad3008bf8d18b15890ef
Luca <luca.fumagalli97@gmail.com>
2022-12-30 18:18:20 +0100

    magari va

--- Comics_Novegro.py
+++ Comics_Novegro.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import geopy
+from geopy.geocoders import Nominatim
 
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
