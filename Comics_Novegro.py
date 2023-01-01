d60247ea2a191cb0d1c8fdb2f0c64575f417cc4e
Luca <luca.fumagalli97@gmail.com>
2023-01-01 21:03:10 +0100

    Gestione Presenze

--- Comics_Novegro.py
+++ Comics_Novegro.py
@@ -47,7 +47,8 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button("Aggiungi Presenza"):
         with open(path+"presenze.csv", "a") as file1:
-            file1.write(infopres)
+            for line in infopres:
+                file1.write(line)
 
 with col2:
     if st.button("Rimuovi Presenza"):
