8981896ee86ab4d07d8dd5a2eb8e7d1189eabf32
Luca <luca.fumagalli97@gmail.com>
2023-01-14 09:51:14 +0100

    Test sulla tabella

--- Comics_Novegro.py
+++ Comics_Novegro.py
@@ -210,19 +210,39 @@ if st.button("Vedi Presenze"):
     st.write(presenze)
 
 
-#from git import Repo
+#provo a inserire la tabella nel memory state
+import streamlit as st
+import pandas as pd
 
-#PATH_OF_GIT_REPO = r'path\to\your\project\folder\.git'  # make sure .git folder is properly configured
-#COMMIT_MESSAGE = 'comment from python script'
+if "mdf" not in st.session_state:
+    st.session_state.mdf = pd.DataFrame(columns=['Location', 'X', 'Y', 'Demand', 'From', 'To', 'Service', 'Vehicle', 'Capacity'])
 
-#def git_push():
-#    try:
-#        repo = Repo(PATH_OF_GIT_REPO)
-#        repo.git.add(update=True)
-#        repo.index.commit(COMMIT_MESSAGE)
-#        origin = repo.remote(name='origin')
-#        origin.push()
-#    except:
-#        print('Some error occured while pushing the code')    
+col0, col1, col2, col3, col4, col5, col6, col7, col8, col9= st.columns(10)
+idx= col0.text_input('Idx')
+location = col1.text_input('Name')
+x = col2.text_input('X Coo' )
+y = col3.text_input('Y Coo' )
+demand = col4.text_input('Demand' )
+from_ = col5.text_input('From')
+to = col6.text_input('To' )
+service = col7.text_input('Service' )
+vehicle = col8.text_input('Vehicle' )
+capacity = col9.text_input('Capacity' )
 
-#git_push()
+run = st.button('Submit')
+
+df_new = pd.DataFrame({'Location': location, 
+                            'X': x, 
+                            'Y': y, 
+                            'Demand': demand, 
+                            'From': from_, 
+                            'To': to,
+                            'Service': service, 
+                            'Vehicle': vehicle, 
+                            'Capacity': capacity}, index=[idx])    
+        
+if run:
+    st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
+    st.dataframe(st.session_state.mdf)
+
+st.write(f"Total Rows: {st.session_state.mdf.shape[0]}")
