2b3ea5404fb2639e4d16e7cec46a0f791bd53b3a
Luca <luca.fumagalli97@gmail.com>
2023-01-14 23:08:28 +0100

    Test con get

--- Comics_Novegro.py
+++ Comics_Novegro.py
@@ -237,7 +237,7 @@ df_new = pd.DataFrame({'Nome': list(dizionario.keys()), 'Data': list(dizionario.
    
         
 if run:
-    st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
+    st.session_state.mdf =st.session_state.get(pd.concat([st.session_state.mdf, df_new], axis=0))
 
 if delete:
     st.session_state.mdf=st.session_state.mdf.loc[(st.session_state.mdf["Nome"] != nome) & (st.session_state.mdf["Data"] != data_def)]
