4cc83974a081fae45fd0aa69016c225042efd8f8
Luca <luca.fumagalli97@gmail.com>
2023-01-14 09:56:24 +0100

    Test sulle presenze

--- Comics_Novegro.py
+++ Comics_Novegro.py
@@ -189,60 +189,22 @@ infopres=pd.DataFrame(info_dict,index=[1])
 
 
 
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("Aggiungi Presenza"):
-        infopres.to_csv(path+"presenze_novegro.csv", mode='a', index = False, header=False)
+col0, col1= st.columns(10)
 
-with col2:
-    if st.button("Elimina Presenza"):   
-        old=pd.read_csv(path+"presenze_novegro.csv")
-        old2=old[(old["Nome"] !=add_e) & (old["Data"] !=add_p)]
-        new=pd.DataFrame(columns=["Nome","Data"])
-        final=pd.concat([new,old2] , ignore_index=True)
-        final.to_csv(path+"presenze_novegro.csv", mode='w', index = False, header=True)
-                
-
-
-
-if st.button("Vedi Presenze"): 
-    presenze=pd.read_csv(path+"presenze_novegro.csv")
-    st.write(presenze)
-
-
-#provo a inserire la tabella nel memory state
-import streamlit as st
-import pandas as pd
-
-if "mdf" not in st.session_state:
-    st.session_state.mdf = pd.DataFrame(columns=['Location', 'X', 'Y', 'Demand', 'From', 'To', 'Service', 'Vehicle', 'Capacity'])
-
-col0, col1, col2, col3, col4, col5, col6, col7, col8, col9= st.columns(10)
-idx= col0.text_input('Idx')
-location = col1.text_input('Name')
-x = col2.text_input('X Coo' )
-y = col3.text_input('Y Coo' )
-demand = col4.text_input('Demand' )
-from_ = col5.text_input('From')
-to = col6.text_input('To' )
-service = col7.text_input('Service' )
-vehicle = col8.text_input('Vehicle' )
-capacity = col9.text_input('Capacity' )
+nome = col0.text_input('Nome' )
+data = col1.text_input('Data' )
 
 run = st.button('Submit')
+                
 
-df_new = pd.DataFrame({'Location': location, 
-                            'X': x, 
-                            'Y': y, 
-                            'Demand': demand, 
-                            'From': from_, 
-                            'To': to,
-                            'Service': service, 
-                            'Vehicle': vehicle, 
-                            'Capacity': capacity}, index=[idx])    
+if "mdf" not in st.session_state:
+    st.session_state.mdf = pd.DataFrame(columns=['Nome', 'Data'])
+dizionario = {nome:data}
+df_new = pd.DataFrame({'Nome': list(dizionario.values()), 'Data': list(dizionario.keys())})
+   
         
 if run:
     st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
     st.dataframe(st.session_state.mdf)
 
-st.write(f"Total Rows: {st.session_state.mdf.shape[0]}")
+st.write(st.session_state.mdf)
