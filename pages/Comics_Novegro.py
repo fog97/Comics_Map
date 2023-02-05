9f65a2e1e72eaf279214a02f485d4d1bf8c0c87d
Luca <luca.fumagalli97@gmail.com>
2023-02-05 21:09:42 +0100

    Nuova Mail

--- pages/Comics_Novegro.py
+++ pages/Comics_Novegro.py
@@ -18,7 +18,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'INFO': 'info@parcoesposizioninovegro.it'
+        'INFO': 'informazioni@festivaldelfumetto.com'
     }
 )
 
