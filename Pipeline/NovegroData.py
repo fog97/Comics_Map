dbb41f420bd52f8110429eb3260401ce62fc4b69
Luca <luca.fumagalli97@gmail.com>
2023-02-07 21:48:16 +0100

    Test logger

--- Pipeline/NovegroData.py
+++ Pipeline/NovegroData.py
@@ -10,52 +10,63 @@ from tqdm import tqdm
 import pandas as pd
 import json
 import pprint
-#Download dei dati di novegro
-########################################################################################################################
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+import logging
+import traceback
 
-wd = webdriver.Chrome('chromedriver',options=options)
 
-wd.get("https://parcoesposizioninovegro.it/biglietti/festival-del-fumetto-web-1giorno-198372-2/")
-list_projects = wd.find_elements(by=By.CSS_SELECTOR, value="p")
-if len(list_projects)>0:
-    ## Costo e tipologia dei biglietti
-    biglietti=pd.DataFrame(columns=["Tipologia","Prezzo"])
-    for nome in list_projects:
-        testo=nome.text
-        test=testo.split(" ")
-        if (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]!="ridotto" and len(test)>7 and test[1]!="gratuito:" :
-            tipologia=test[0]+" "+test[1]+" "+test[2]+" "+test[3].replace(":","")
-            prezzo=test[4]+" "+test[6]+" "+test[7]
-            row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
-            biglietti=pd.concat([biglietti,row])
-        elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]!="ridotto" and test[1]!="gratuito:" :
-            tipologia=test[0]+" "+test[2]+" "+test[3]+" "+test[4].replace(":","")
-            prezzo=test[5]+" "
-            row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
-            biglietti=pd.concat([biglietti,row])
-        elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="ridotto" and len(test)>8 and test[1]!="gratuito:" :
-            tipologia=test[0]+" "+test[1]+" "+test[2]+" "+test[3]+" "+test[4].replace("*","").replace(":","")
-            prezzo=test[5]+"  "+test[7]+"  "+test[8]
-            row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
-            biglietti=pd.concat([biglietti,row])
-        elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="ridotto" and test[1]!="gratuito:" :
-            tipologia=test[0]+" "+test[1]+" "+test[3]+" "+test[3]+" "+test[5].replace("*","").replace(":","")
-            prezzo=test[6]
-            row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
-            biglietti=pd.concat([biglietti,row])
-        elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="gratuito:" :
-            tipologia=test[0]+" "+test[1].replace("*","").replace(":","")+" "+test[4]+" "+test[6]+"-"+test[8]+" "+test[9]+" e "+test[12]+" con "+test[14]+" "+test[16].replace("l’","")+"-"+test[19].replace(".","")+ " e accompagnatore"
-            prezzo="0"
-            row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])    
-            biglietti=pd.concat([biglietti,row])
+try:
+    #Download dei dati di novegro
+    ########################################################################################################################
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
 
-    biglietti.to_csv("C:/Users/lucaf/OneDrive/Desktop/Esercizi/Comics_Map/Novegro/biglietti_novegro.csv",sep=";")
+    wd = webdriver.Chrome('chromedriver',options=options)
 
-else:
-    biglietti=pd.DataFrame(columns=["Tipologia","Prezzo"])
-    biglietti.to_csv("C:/Users/lucaf/OneDrive/Desktop/Esercizi/Comics_Map/Novegro/biglietti_novegro.csv",sep=";")
+    wd.get("https://parcoesposizioninovegro.it/biglietti/festival-del-fumetto-web-1giorno-198372-2/")
+    list_projects = wd.find_elements(by=By.CSS_SELECTOR, value="p")
+    if len(list_projects)>0:
+        ## Costo e tipologia dei biglietti
+        biglietti=pd.DataFrame(columns=["Tipologia","Prezzo"])
+        for nome in list_projects:
+            testo=nome.text
+            test=testo.split(" ")
+            if (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]!="ridotto" and len(test)>7 and test[1]!="gratuito:" :
+                tipologia=test[0]+" "+test[1]+" "+test[2]+" "+test[3].replace(":","")
+                prezzo=test[4]+" "+test[6]+" "+test[7]
+                row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
+                biglietti=pd.concat([biglietti,row])
+            elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]!="ridotto" and test[1]!="gratuito:" :
+                tipologia=test[0]+" "+test[2]+" "+test[3]+" "+test[4].replace(":","")
+                prezzo=test[5]+" "
+                row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
+                biglietti=pd.concat([biglietti,row])
+            elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="ridotto" and len(test)>8 and test[1]!="gratuito:" :
+                tipologia=test[0]+" "+test[1]+" "+test[2]+" "+test[3]+" "+test[4].replace("*","").replace(":","")
+                prezzo=test[5]+"  "+test[7]+"  "+test[8]
+                row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
+                biglietti=pd.concat([biglietti,row])
+            elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="ridotto" and test[1]!="gratuito:" :
+                tipologia=test[0]+" "+test[1]+" "+test[3]+" "+test[3]+" "+test[5].replace("*","").replace(":","")
+                prezzo=test[6]
+                row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])
+                biglietti=pd.concat([biglietti,row])
+            elif (test[0]=="Abbonamento" or test[0]=="Ingresso") and test[1]=="gratuito:" :
+                tipologia=test[0]+" "+test[1].replace("*","").replace(":","")+" "+test[4]+" "+test[6]+"-"+test[8]+" "+test[9]+" e "+test[12]+" con "+test[14]+" "+test[16].replace("l’","")+"-"+test[19].replace(".","")+ " e accompagnatore"
+                prezzo="0"
+                row=pd.DataFrame([[tipologia,prezzo]],columns=["Tipologia","Prezzo"])    
+                biglietti=pd.concat([biglietti,row])
+
+        biglietti.to_csv("C:/Users/lucaf/OneDrive/Desktop/Esercizi/Comics_Map/Novegro/biglietti_novegro.csv",sep=";")
+
+    else:
+        biglietti=pd.DataFrame(columns=["Tipologia","Prezzo"])
+        biglietti.to_csv("C:/Users/lucaf/OneDrive/Desktop/Esercizi/Comics_Map/Novegro/biglietti_novegro.csv",sep=";")
+except :
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    sam =  traceback.format_exception(exc_type, exc_value,
+                                           exc_traceback)
+    with open("C:/Users/lucaf/OneDrive/Desktop/Esercizi/Comics_Map/Novegro/log_novegro.txt", "w") as file1:
+        file1.write(sam)
