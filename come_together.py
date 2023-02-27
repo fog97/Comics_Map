142186271df0fbec5662dd79b38d6e86c4dcc454
Luca <luca.fumagalli97@gmail.com>
2023-02-27 21:36:03 +0100

    Test authenticator

--- come_together.py
+++ come_together.py
@@ -8,8 +8,41 @@ import shapefile
 from shapely.geometry import Point
 from shapely.geometry import shape
 import pydeck as pdk
+import streamlit_authenticator as stauth
 from PIL import Image
 path='/app/comics_map/'
+
+import streamlit_authenticator as stauth
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
+
+
+
+
+
 
 st.set_page_config(
     page_title="Come Together",
