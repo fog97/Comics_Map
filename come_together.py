77c2da6222242ffa4ebabaa04cc0f836b6a432b2
Luca <luca.fumagalli97@gmail.com>
2023-03-28 21:30:48 +0200

    Test formattazione pagina

--- come_together.py
+++ come_together.py
@@ -41,63 +41,76 @@ if not _RELEASE:
         config['cookie']['expiry_days'],
         config['preauthorized']
     )
+    with st.expander("Nuovo di qui? Registrati!", expanded=False):
+        # Creating a new user registration widget
+        try:
+            if authenticator.register_user('Register user', preauthorization=False):
+                st.success('User registered successfully')
+        except Exception as e:
+            st.error(e)
 
     # creating a login widget
     name, authentication_status, username = authenticator.login('Login', 'main')
     if authentication_status:
         authenticator.logout('Logout', 'main')
         st.write(f'Welcome *{name}*')
+        col1,col2=st.columns(2)
+        with col1:
+            with st.expander("Change Password", expanded=False):
+                    # Creating a password reset widget
+                if authentication_status:
+                    try:
+                        if authenticator.reset_password(username, 'Reset password'):
+                            st.success('Password modified successfully')
+                    except Exception as e:
+                        st.error(e)
+        with col2:
+            with st.expander("Change Password", expanded=False):
+                # Creating an update user details widget
+                if authentication_status:
+                    try:
+                        if authenticator.update_user_details(username, 'Update user details'):
+                            st.success('Entries updated successfully')
+                    except Exception as e:
+                        st.error(e)
+
     elif authentication_status is False:
         st.error('Username/password is incorrect')
     elif authentication_status is None:
         st.warning('Please enter your username and password')
+        col1,col2=st.columns(2)
+        with col1:
+            with st.expander("Password dimenticata?", expanded=False):
+                # Creating a forgot password widget
+                try:
+                    username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+                    if username_forgot_pw:
+                        st.success('New password sent securely')
+                        st.write(random_password)
+                        # Random password to be transferred to user securely
+                    else:
+                        st.error('Username not found')
+                except Exception as e:
+                    st.error(e)
+        with col2:
+            with st.expander("Username Dimenticato?", expanded=False):
+                # Creating a forgot username widget
+                try:
+                    username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
+                    if username_forgot_username:
+                        st.success('Username sent securely')
+                        st.success(email_forgot_username)
+                        # Username to be transferred to user securely
+                    else:
+                        st.error('Email not found')
+                except Exception as e:
+                    st.error(e)
 
-    # Creating a password reset widget
-    if authentication_status:
-        try:
-            if authenticator.reset_password(username, 'Reset password'):
-                st.success('Password modified successfully')
-        except Exception as e:
-            st.error(e)
 
-    # Creating a new user registration widget
-    try:
-        if authenticator.register_user('Register user', preauthorization=False):
-            st.success('User registered successfully')
-    except Exception as e:
-        st.error(e)
 
-    # Creating a forgot password widget
-    try:
-        username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
-        if username_forgot_pw:
-            st.success('New password sent securely')
-            st.write(random_password)
-            # Random password to be transferred to user securely
-        else:
-            st.error('Username not found')
-    except Exception as e:
-        st.error(e)
 
-    # Creating a forgot username widget
-    try:
-        username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
-        if username_forgot_username:
-            st.success('Username sent securely')
-            st.success(email_forgot_username)
-            # Username to be transferred to user securely
-        else:
-            st.error('Email not found')
-    except Exception as e:
-        st.error(e)
 
-    # Creating an update user details widget
-    if authentication_status:
-        try:
-            if authenticator.update_user_details(username, 'Update user details'):
-                st.success('Entries updated successfully')
-        except Exception as e:
-            st.error(e)
+
 
     # Saving config file
     with open(path+'config.yaml', 'w') as file:
