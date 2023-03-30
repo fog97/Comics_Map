dfb73ff99d43dbfcc42f75077bf6702489127239
Fuma97 <59887242+fog97@users.noreply.github.com>
2023-03-30 12:46:37 +0200

    Non capisco gli apici

--- come_together.py
+++ come_together.py
@@ -63,8 +63,7 @@ if not _RELEASE:
                 from github import Github
                 # Replace <ACCESS_TOKEN> with your personal access token
                 ACCESS_TOKEN = st.secrets["git"]["git_pwd"]
-                st.write(st.secrets["mongo"]["db_pswd"])
-                st.write(st.secrets["git"]["git_pwd"])
+                st.write(st.secrets["git"]["git_pwd"].strip())
                 # Replace <REPO_NAME> with the name of the repository where you want to push the file
                 REPO_NAME = 'fog97/Comics_Map'
                 # Replace <FILE_NAME> with the name of the file you want to push
