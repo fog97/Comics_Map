70e724077993c0667fd35acd6963dbc84ec5860f
Luca <luca.fumagalli97@gmail.com>
2023-04-26 22:45:57 +0200

    TEst update

--- come_together.py
+++ come_together.py
@@ -59,7 +59,7 @@ if not _RELEASE:
         try:
             if authenticator.register_user('Register user', preauthorization=False):
                 st.success('User registered successfully')
-                filter = { 'id': "ObjectId('6449829227d38cc5468cdafa')" }
+                filter = { 'id':config["_id"]}
                 st.write(config)
                 collection.replace_one(filter, config)
         except Exception as e:
