a93b40db653f947c65f1f17411060aaeebfc1043
Fuma97 <59887242+fog97@users.noreply.github.com>
2023-05-04 10:20:23 +0200

    test

--- come_together.py
+++ come_together.py
@@ -93,7 +93,7 @@ if not _RELEASE:
         collection_friends = db.Friends
         filter_friends = { 'user': st.session_state.utente }
         security =pd.DataFrame(list(collection_secs.find(filter_friends)))
-        st.write(security)
+        st.write(len(security))
         try:
             friends =collection_friends.find(filter_friends)
             fr=pd.DataFrame(list(friends))
