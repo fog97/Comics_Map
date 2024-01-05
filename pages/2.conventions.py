b47e771cf313d6c79ca602fa9f4f257986251578
Luca <luca.fumagalli97@gmail.com>
2024-01-05 13:05:05 +0100

    Test

--- pages/2.conventions.py
+++ pages/2.conventions.py
@@ -161,9 +161,8 @@ if st.session_state.autenticazione:
     Conv_Selector=partecipazioni_keys["Nome_Conv"].unique()[0]
     Conv_Selector= st.selectbox('Convention Disponibili',partecipazioni_keys["Nome_Conv"].unique())
     st.write(Conv_Selector)
-    st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector]["Partecipanti"])
+    st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector]["Partecipanti"][0])
     st.write(partecipazioni_keys["Partecipanti"])
-    st.write(partecipazioni_keys)
     col1, col2 = st.columns((10, 10))
     with col1:
         do_action = st.button(key='1a',label="Conferma Presenza")
