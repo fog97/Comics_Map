cfd910198f22e3870b95e5d148577099a5313466
Luca <luca.fumagalli97@gmail.com>
2024-01-05 11:51:46 +0100

    Test bug

--- pages/2.conventions.py
+++ pages/2.conventions.py
@@ -151,9 +151,9 @@ if st.session_state.autenticazione:
         st.markdown("*Refresh della pagina per verificare l'effettiva cancellazione*")
 
     lista_conv=[]
-    partecipazioni_keys=presenze.copy()
+    partecipazioni_keys = pd.DataFrame(list(db["Convention"].find()))
     partecipazioni_keys["Nome_Conv"]=partecipazioni_keys["Organizzatore"]+' -- '+partecipazioni_keys["Titolo"]
-
+    st.table(partecipazioni_keys)
     
     st.markdown("**Conferma Partecipazione**")
     st.write("Selezione la Convention")
