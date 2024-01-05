0fe10b8a57cc9f10aee5cd9d68ace4cafcd569e1
Luca <luca.fumagalli97@gmail.com>
2024-01-05 13:26:22 +0100

    test

--- pages/2.conventions.py
+++ pages/2.conventions.py
@@ -164,8 +164,8 @@ if st.session_state.autenticazione:
     st.write(partecipazioni_keys["Partecipanti"])
     st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector])  
     st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector].index[0]) 
-    st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector][partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector].index[0]]) 
-
+    st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector]["Partecipanti"]) 
+    st.write(partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector]["Partecipanti"][partecipazioni_keys[partecipazioni_keys.Nome_Conv==Conv_Selector].index[0]])
     col1, col2 = st.columns((10, 10))
     with col1:
         do_action = st.button(key='1a',label="Conferma Presenza")
